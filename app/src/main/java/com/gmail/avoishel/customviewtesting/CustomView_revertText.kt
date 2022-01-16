4f720f02d1dc8a0d31e0da101c9efbab8cbeecf0
Alexandr Voishel <avoishel@gmail.com>
2022-01-16 23:40:35 +0300

    add: CustomView.class

---
+++ app/src/main/java/com/gmail/avoishel/customviewtesting/CustomView_revertText.kt
@@ -0,0 +1,12 @@
+package com.gmail.avoishel.customviewtesting
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class CustomView_revertText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defaultAttrs: Int = 0
+) : AppCompatTextView(context, attrs, defaultAttrs) {
+}
