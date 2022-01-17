7b79de8601a71dc8f711ee2a67265129a58ad492
Alexandr Voishel <avoishel@gmail.com>
2022-01-17 15:56:18 +0300

    add: face drown by Canvas

--- app/src/main/java/com/gmail/avoishel/customviewtesting/MainActivity.kt
+++ app/src/main/java/com/gmail/avoishel/customviewtesting/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.gmail.avoishel.customviewtesting.UI.CustomView_revertText
+import com.gmail.avoishel.customviewtesting.UI.CustonView_face
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,16 +12,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(CustonView_face(this))
 
-        textviewHelloWorld = findViewById(R.id.helloWorldTextView)
+        //textviewHelloWorld = findViewById(R.id.helloWorldTextView)
     }
 
-    fun revertTextClick(view: android.view.View) {
-        if (textviewHelloWorld.shouldRevertText == true){
-            textviewHelloWorld.setRevertText(false)
-        } else {
-            textviewHelloWorld.setRevertText(true)
-        }
-    }
+//    fun revertTextClick(view: android.view.View) {
+//        if (textviewHelloWorld.shouldRevertText == true){
+//            textviewHelloWorld.setRevertText(false)
+//        } else {
+//            textviewHelloWorld.setRevertText(true)
+//        }
+//    }
 }
